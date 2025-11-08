b9dfff3530d4bef51b372c2065aeb4dc03e23655
Ivan <ivandelllanoblanco@yahoo.es>
2025-11-08 14:51:09 +0100

    add widget grid placeholder to dashboard

---
+++ src/app/features/dashboard/components/widget-grid.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-widget-grid',
+  imports: [],
+  templateUrl: './widget-grid.html',
+  styleUrl: './widget-grid.scss',
+})
+export class WidgetGrid {
+
+}
--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {ThemeService} from '../../core/services/theme.service';
 import { CommonModule } from '@angular/common';
+import { WidgetGrid } from './components/widget-grid';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
   standalone: true,
+  imports: [WidgetGrid],
 })
 export class Dashboard 
 {
